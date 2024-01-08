189511568feb0294663858e33a6083621a1674ca
Soo Jung Kim <soojung.kimm@gmail.com>
2024-01-08 14:20:50 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+let names = ["Charlie", "Samip", "Ali"];
+function writeCards(names, event){
+    let messages = [];
+    let i = 0;
+    for(i = 0; i< names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+console.log(writeCards(names, "birthday"));
+
+
+
+function countDown(num){
+    let i = 10;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+console.log(countDown());
